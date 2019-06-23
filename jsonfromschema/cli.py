58885a1591a0edf13693aaffd68a9d816b605383
Michał Łabędzki <michal.tomasz.labedzki@gmail.com>
2019-06-23 14:02:37 +0200

    add most basic functionality

--- jsonfromschema/cli.py
+++ jsonfromschema/cli.py
@@ -1,9 +1,34 @@
 import argparse
+import json
 import sys
+import jsonfromschema.lib
+
+from pprint import pprint
+
 
 def main(args=sys.argv[1:]):
     parser = argparse.ArgumentParser(description='Generate JSON data file (*.json) from JSON Schema')
     parser.add_argument('schema', type=str, help='path to JSON Schema file')
     parser.add_argument('output', type=str, help='path to JSON data output file')
+    parser.add_argument('-v', '--verbose', action="store_true", help='verbose mode')
     args = parser.parse_args()
+    print('schema', args.schema)
+    print('output', args.output)
+
+    with open(args.schema, 'r') as input:
+        schema = json.load(input)
+        if args.verbose:
+            print('>>> Schema is:')
+            pprint(schema)
+    input.close()
+
+    with open(args.output, 'w') as output:
+        output_dict = jsonfromschema.lib.generate_dict(schema)
+        if args.verbose:
+            print('>>> Output is:')
+            pprint(output_dict)
+        output_json = json.dumps(output_dict)
+        output.write(output_json)
+    output.close()
+    
     sys.exit(0) 
---
+++ jsonfromschema/lib.py
@@ -0,0 +1,82 @@
+import math
+import sys
+
+
+def generate_dict(schema_object):
+    data = {}
+    for property_name in schema_object['properties']:
+        property = schema_object['properties'][property_name]
+        property_type = property['type']
+
+        if 'default' in property:
+            data[property_name] = property['default']
+            continue
+        
+        if 'enum' in property:
+            data[property_name] = property['enum'][0]
+            continue
+
+        if property_type == 'string':
+            data[property_name] = ""
+
+            if 'minLength' in property:
+                data[property_name] = 'a' * property['minLength']
+
+            # TODO pattern
+            # TODO format
+        elif property_type == 'integer':
+            data[property_name] = 0
+
+            if 'multipleOf' in property:
+                data[property_name] = property['multipleOf']
+            
+            if 'minimum' in property:
+                data[property_name] = property['minimum']
+            
+            if 'exclusiveMinimum' in property:
+                data[property_name] = property['exclusiveMinimum'] + 1
+            
+            if 'exclusiveMinimum' is True: # draft-4
+                data[property_name] += 1
+                # TODO check invalid combination of *minimum/*maximum/multiple
+                #if 'maximum' in property and data[property_name] > property['maximum']:
+                #    raise Exception('')
+        elif property_type == 'number':
+            data[property_name] = 0.0
+    
+            if 'multipleOf' in property:
+                data[property_name] = property['multipleOf']
+
+            if 'minimum' in property:
+                data[property_name] = property['minimum']
+            
+            if 'exclusiveMinimum' in property and 'exclusiveMinimum' is not False:
+                if 'exclusiveMinimum' is True: # draft-4
+                    exclusive_minimum = property['minimum']
+                else:
+                    exclusive_minimum = property['exclusiveMinimum']
+                m, e = math.frexp(exclusive_minimum)
+                value =  (m + sys.float_info.epsilon) * 2 ** e
+                if 'multipleOf' in property:
+                    multiple_of = property['multipleOf']
+                    value = math.ceil(value / multiple_of) * multiple_of
+                
+                data[property_name] = value   
+            # TODO check invalid combination of *minimum/*maximum/multiple
+        elif property_type == 'object':
+            data[property_name] = generate_dict(property)
+        elif property_type == 'array':
+            data[property_name] = [0]
+            # TODO items one
+            # TODO items list
+            # TODO contains
+            # TODO minItems
+            # TODO uniqueItems
+        elif property_type == 'boolean':
+            data[property_name] = False
+        elif property_type == 'null':
+            data[property_name] = None
+        else:
+            raise Exception(f'Not supported type: {property_type} in property "{property_name}"')
+
+    return data
